356364959a9d7cceb82949163d358083bf08604d
informatica7 <informatica7@inacap.cl>
2025-11-13 15:00:20 -0300

    cambios realizados

--- echomusicApp/models.py
+++ echomusicApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=30, null=False, blank=False)
+    apellido = models.CharField(max_length=30, null=False, blank=False)
+    username = models.CharField(max_length=30, null=False, blank=False, unique=True)
+    correo = models.EmailField(null=False, blank=False, unique=True)
+    contraseña = models.CharField(max_length=50, null=False, blank=False)
+
+class Playlist(models.Model):
+    nombre = models.CharField(max_length=30, null=False, blank=False, unique=True)
+    descripcion = models.TextField(max_length=500, null=False, blank=False)
+    imagen = models.ImageField(null=True, blank=True, default='/static/images/icons/img_playlist.svg')
+
+class Cancion(models.Model):
+    nombre = models.CharField(max_length=30, null=False, blank=False, unique=True)
+    autor = models.CharField(max_length=30, null=False, blank=False)
+    archivo = models.FileField(null=False, blank=False)
