5779fba0311ca4137c883028614395b89766d83f
informatica1 <informatica1@inacap.cl>
2025-11-13 10:52:18 -0300

    Cambios realizados

--- EchoMusic/settings.py
+++ EchoMusic/settings.py
@@ -77,6 +77,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'usuarios':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'echomusic_bd',
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
 
--- echomusicApp/views.py
+++ echomusicApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def login(request):
+    return render(request, 'login.html')
+
+def index(request):
+    if 'usuario_id' in request.session:
+        return render(request, 'index.html')
+    else:
+        return redirect('login')
