9441e49eadff07850c2991c035470daff5c33532
Felipe Latorre <renojaker@gmail.com>
2025-11-17 16:53:34 -0300

    .

---
+++ static/scripts/index.js
@@ -0,0 +1,6 @@
+const btn_subir = document.getElementById("btn_subir")
+const div_fondo_subir = document.getElementById("div_fondo_subir")
+
+btn_subir.addEventListener("click", function(){
+    div_fondo_subir.style.display = "grid"
+})
